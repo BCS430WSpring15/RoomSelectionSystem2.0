1a6e8f0803bbccba0b5d45b59f90b08d61a80ce6
Jairo Veloz <jpavelw@gmail.com>
2015-04-22 20:27:24 -0400

    Login screen modified

--- index.php
+++ index.php
@@ -1,80 +1,54 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-	
+
 <head>
-	<title>The Login-Animated Website Template | Home :: w3layouts</title>
-		<meta charset="utf-8">
-		<link href="css/style.css" rel='stylesheet' type='text/css' />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-		<!--webfonts-->
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,300,600,700' rel='stylesheet' type='text/css'>
-		<!--//webfonts-->
+  <title>Room Selection System</title>
+  <meta name="description" content="website description" />
+  <meta name="keywords" content="website keywords, website keywords" />
+  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+  <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+  <!--webfonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,300,600,700' rel='stylesheet' type='text/css'>
+    <!--//webfonts-->
+  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
 </head>
+
 <body>
-	
-				 <!-----start-main---->
-				<div class="login-form">
-					<div class="head">
-						<img src="images/mem2.jpg" alt=""/>
-						
-					</div>
-				<form>
-					<li>
-						<input type="text" class="text" value="USERNAME" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'USERNAME';}" ><a href="#" class=" icon user"></a>
-					</li>
-					<li>
-						<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"><a href="#" class=" icon lock"></a>
-					</li>
-					<div class="p-container">
-								<label class="checkbox"><input type="checkbox" name="checkbox" checked><i></i>Remember Me</label>
-								<input type="submit" onclick="myFunction()" value="SIGN IN" >
-							<div class="clear"> </div>
-					</div>
-				</form>
-			</div>
-			<?php
-$servername = "localhost";
-$username = "root";
-$password = "Jpvv1912*";
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.html">Room<span class="logo_colour">Selection System</span></a></h1>
+          <h2>Simple. Contemporary.</h2>
+        </div>
+      </div>
+    </div>
+    
+    <!-----start-main---->
+        <div class="login-form">
+          <div class="head">
+            <img src="images/mem2.jpg" alt=""/>
+            
+          </div>
+        <form>
+          <li>
+            <input type="text" class="text" value="USERNAME" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'USERNAME';}" ><a href="#" class=" icon user"></a>
+          </li>
+          <li>
+            <input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"><a href="#" class=" icon lock"></a>
+          </li>
+          <div class="p-container">
+              <input type="submit" onclick="myFunction()" value="SIGN IN" >
+            <div class="clear"> </div>
+          </div>
+        </form>
+      </div>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-$db_selected = $conn->query("USE RSS");
-if (!$db_selected) {
-    die ('Can\'t use RSS : ' . mysql_error());
-}
-$sql = "SELECT * FROM `SUITE` WHERE 1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br>id: " . $row["SuiteID"]. " - Name: " . $row["SuiteType"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
- $conn->close();
-?> 
-			<!--//End-login-form-->
-		  <!-----start-copyright---->
-   					<div class="copy-right">
-						<p>Template by <a href="http://w3layouts.com">w3layouts</a></p> 
-					</div>
-				<!-----//end-copyright---->
-		 		
+    <div id="content_footer"></div>
+    <div id="footer">
+      Copyright &copy; Eagle Solutions | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+    </div>
+  </div>
 </body>
 </html>
