e2e32fd20dde1c65fd90394ed621f9d2a08702e1
Angelo Martino <marta3@farmingdale.edu>
2015-04-27 15:22:00 -0400

    logo added to login screen

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php
 	function getCredentials(){
-		$credentialsInfo = array('servername' => "localhost", 'username' => "root", 'password' => "Jpvv1912*");
+		$credentialsInfo = array('servername' => "localhost", 'username' => "root", 'password' => "root");
 		return $credentialsInfo;
 	}
 ?>
